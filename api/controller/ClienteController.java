a5aff774f2ebb976cce431ec20436d1746ac7fa0
Matheus Siqueira <matheus.siqsantos05@gmail.com>
2026-01-28 17:14:28 -0300

    Término da fase 2

--- api/controller/ClienteController.java
+++ api/controller/ClienteController.java
@@ -1,13 +1,24 @@
 package br.dev.matheus.OSApiApplication.api.controller;
 
+import br.dev.matheus.OSApiApplication.domain.model.Cliente;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
     
+    List<Cliente> listaClientes;
+    
     @GetMapping("/clientes")
-    public String listas() {
-        return "teste api";
+    public List<Cliente> listas() {
+        
+        listaClientes = new ArrayList<Cliente>();
+        listaClientes.add(new Cliente (1, "Neymar", "Ney@santosfc.com", "15-99443-4500"));
+        listaClientes.add(new Cliente (1, "Messi", "Lionel@intermiamisc", "34-96453-0231"));
+        listaClientes.add(new Cliente (1, "Suarez", "Luisito@gremiofc", "92-98843-8429"));
+        
+        return listaClientes;
     }
 }
